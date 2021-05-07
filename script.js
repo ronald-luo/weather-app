6b95ed0f555158f678590dd561a3b88854994826
ronald-luo <r.luo.180@gmail.com>
2021-05-07 17:28:56 -0400

    changing fonts, adding favicon, reducing code

--- script.js
+++ script.js
@@ -1,56 +1,29 @@
 // form logic:
 things = [{ inquiry: "toronto" }]
 
-const doSomething = (ev) => {
+const handleForm = (ev) => {
     ev.preventDefault() // stops form submission
     let search = {
         inquiry: document.getElementById("user-search").value
     }
 
-
     things.unshift(search) // add search to spot one and push everything donw
 
     document.forms[0].reset() // clears form for next entry
 
-    temp_location = things[0].inquiry // updates our new location
-
-    // Update DOM to new 
-    weather(temp_location).then((data) => {
-        // console.log(data)
-
-        // uppercasing the description
-        let descrip_cap = data.weather[0].description
-        descrip_cap = descrip_cap[0].toUpperCase() + descrip_cap.substring(1)
-        document.getElementById("location").innerHTML = `${data.name}`
-        document.getElementById("temperature").innerHTML = `${data.main.temp} °C`
-        document.getElementById("description").innerHTML = descrip_cap
-        document.getElementById("feels").innerHTML = `Feels like: ${data.main.feels_like} °C`
-        document.getElementById("wind").innerHTML = `Wind: ${data.wind.speed} m/s`
-    })
+    updateLocation()
 
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('btn').addEventListener('click', doSomething)
+    document.getElementById('btn').addEventListener('click', handleForm)
 })
-// console.log(things)
 
-// update temp_location to new search entry
-temp_location = things[0].inquiry
+updateLocation()
 
-weather(temp_location).then((data) => {
-    // console.log(data)
 
-    // uppercasing the description
-    let descrip_cap = data.weather[0].description
-    descrip_cap = descrip_cap[0].toUpperCase() + descrip_cap.substring(1)
 
-    document.getElementById("location").innerHTML = `${data.name}`
-    document.getElementById("temperature").innerHTML = `${data.main.temp} °C`
-    document.getElementById("description").innerHTML = descrip_cap
-    document.getElementById("feels").innerHTML = `Feels like: ${data.main.feels_like} °C`
-    document.getElementById("wind").innerHTML = `Wind: ${data.wind.speed} m/s`
-})
+
 
 // getting data from server
 async function weather(location) {
@@ -59,4 +32,24 @@ async function weather(location) {
     let data = await response.json()
     // console.log(data.weather[0].description)
     return Object.assign({}, data)
+}
+
+function updateLocation() {
+
+    // update temp_location to new search entry
+    temp_location = things[0].inquiry
+
+    weather(temp_location).then((data) => {
+        // console.log(data)
+
+        // uppercasing the description
+        let descrip_cap = data.weather[0].description
+        descrip_cap = descrip_cap[0].toUpperCase() + descrip_cap.substring(1)
+
+        document.getElementById("location").innerHTML = `${data.name}`
+        document.getElementById("temperature").innerHTML = `${data.main.temp} °C`
+        document.getElementById("description").innerHTML = descrip_cap
+        document.getElementById("feels").innerHTML = `Feels like: ${data.main.feels_like} °C`
+        document.getElementById("wind").innerHTML = `Wind: ${data.wind.speed} m/s`
+    })
 }
